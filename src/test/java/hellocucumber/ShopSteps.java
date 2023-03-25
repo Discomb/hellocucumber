0f601b9574f53866372db2e789fb63f96417f98d
Discomb <lllkoutalll@gmail.com>
2023-03-25 17:12:13 +0300

    Added new test scenario. Sidebar menu header checker

--- src/test/java/hellocucumber/ShopSteps.java
+++ src/test/java/hellocucumber/ShopSteps.java
@@ -6,36 +6,31 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 public class ShopSteps {
+
+
+
     private WebDriver driver;
 
-//    @Before
-//    public void startBrowser() throws InterruptedException {
-//        System.setProperty("webdriver.chrome.driver", "C:/Tools/chromedriver.exe");
-//        System.setProperty("webdriver.http.factory", "jdk-http-client");
-//
-//        driver = new ChromeDriver();
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-//        ChromeOptions chromeOptions = new ChromeOptions();
-//        chromeOptions.addArguments("--remote-allow-origins=*");
-//
-//    }
+        @Before
+    public void startBrowser() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:/Tools/chromedriver.exe");
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*");
+
+    }
 
 //    @Before
 //    public void startBrowser(){
@@ -54,28 +49,37 @@ public class ShopSteps {
 //
 //    }
 
-    /*TODO: Implement Firefox running*/
+//    @Before
+//    public void setup(){
+//        System.setProperty("webdriver.gecko.driver", "C:/Tools/geckodriver.exe");
+//
+//        FirefoxOptions ffOptions = new FirefoxOptions();
+//        ffOptions.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+//
+//        driver = new FirefoxDriver(ffOptions);
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//
+//    }
 
-    @Before
-    public void startBrowser(){
-        System.setProperty("webdriver.gecko.driver", "C:/Tools/geckodriver.exe");
 
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setBrowserName("MicrosoftEdge");
-//        capabilities.setPlatform(Platform.WINDOWS);
-//        capabilities.setVersion("111.0.1661.51");
-
-        FirefoxOptions ffOptions = new FirefoxOptions();
-        ffOptions.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
-
-        driver = new FirefoxDriver(ffOptions);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-    }
 
     @Given("I am on the shop admin login page")
     public void I_visit_admin_page() {
         driver.get("http://localhost/litecart/admin/");
+    }
+
+    @Given ("I am logged into shop admin page")
+    public void login_admin(){
+            I_visit_admin_page();
+            enter_creds();
+            checkTitle();
+    }
+
+    @When ("I go through menu")
+    public List<WebElement> get_menu_elements(){
+            List<WebElement> menuList = driver.findElements(By.cssSelector("div#sidebar li a"));
+
+            return menuList;
     }
 
     @When("I enter credentials")
@@ -94,12 +98,32 @@ public class ShopSteps {
 
     @Then("I am logged in")
     public void checkTitle() {
-        WebElement element = driver.findElement(By.xpath("//*[@id='box-apps-menu']/li[1]/a"));
+            WebElement element = driver.findElement(By.xpath("//*[@id='box-apps-menu']/li[1]/a"));
         element.click();
     }
+
+    @Then ("Every title has a header")
+    public void checkHeaders() {
+        List<WebElement> elements = get_menu_elements();
+
+            for (int i = 0; i < elements.size(); i++ ){
+                elements = get_menu_elements();
+                WebElement element = elements.get(i);
+//                System.out.println(element.findElement(By.cssSelector("span.name")).getText());
+                element.click();
+//                synchronized (driver){
+//                    try {driver.wait(3000); }
+//                catch (InterruptedException e) {
+//                    System.out.println(e.getMessage());
+//                }}
+                assert driver.findElement(By.cssSelector(".card-header")).isDisplayed();
+            }
+    }
+
 
     @After
     public void close(){
         driver.quit();
     }
+
 }
--- target/test-classes/hellocucumber/shopLogin.feature
+++ target/test-classes/hellocucumber/shopLogin.feature
@@ -1,6 +1,11 @@
-Feature: Log into shop admin page
+Feature: Shop admin page
 
-  Scenario: Log into admin page
+  Scenario: 01 - Log into admin page
     Given I am on the shop admin login page
     When I enter credentials
     Then I am logged in
+
+  Scenario: 02 - Go through menu
+    Given I am logged into shop admin page
+    When I go through menu
+    Then Every title has a header
